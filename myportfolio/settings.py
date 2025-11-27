20dadf89b545376332f8d5b2db1fc55523ee928f
rajkumar789 <rajkumarsoni280@gmail.com>
2025-11-27 01:27:40 +0000

    Fix syntax error in settings.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',  # Add WhiteNoise
     'django.middleware.gzip.GZipMiddleware', # Enable GZip Compression
